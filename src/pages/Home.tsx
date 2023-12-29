eacec0e9445f81773aa2167d52e97df5a7e79ef3
ducksoop <torresj1997@outlook.com>
2023-12-29 10:14:38 -0600

    feat: create styling for the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
+import { AiOutlineSearch } from 'react-icons/ai';
+import styles from '../styles/Home.module.scss';
+
 interface Props {}
 
 const Home = ({}: Props) => {
-  return <h1>Home</h1>;
+  const [query, setQuery] = useState<string>('');
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Search for a movie</h1>
+        <div className={styles.formContainer}>
+          <div className={styles.inputContainer}>
+            <input
+              type='text'
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+            />
+            <AiOutlineSearch size='26px' color='#ffffff' />
+          </div>
+          <div className={styles.searchResults}>{query}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
